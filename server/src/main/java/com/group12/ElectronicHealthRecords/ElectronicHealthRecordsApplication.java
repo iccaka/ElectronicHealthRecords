75ac33e21012dfa88e1b973ab1e2fdbbcc885a72
Stefan <stefanmitov8@gmail.com>
2021-11-14 10:21:23 +0200

    initialize backend project

---
+++ server/src/main/java/com/group12/ElectronicHealthRecords/ElectronicHealthRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.group12.ElectronicHealthRecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicHealthRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicHealthRecordsApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/group12/ElectronicHealthRecords/ElectronicHealthRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group12.ElectronicHealthRecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicHealthRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
