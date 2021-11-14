02297466b76d82d6f5825c6dd25b98e609aa9c34
ManoelaYordanova <manoela.yordanova99@gmail.com>
2021-11-14 11:13:27 +0200

    create patient and calendar entities

---
+++ server/src/main/java/com/group12/ElectronicHealthRecords/entities/Calendar.java
@@ -0,0 +1,73 @@
+package com.group12.ElectronicHealthRecords.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="calendar")
+public class Calendar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "date", nullable = false)
+    private Date date;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "duration", nullable = false)
+    private Integer duration;
+
+    public Calendar() {
+    }
+
+    public Calendar(Long id,String name, Date date, String description, Integer duration) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.description = description;
+        this.duration = duration;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+}
---
+++ server/src/main/java/com/group12/ElectronicHealthRecords/entities/Patient.java
@@ -0,0 +1,135 @@
+package com.group12.ElectronicHealthRecords.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="patient")
+public class Patient {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Id
+    @Column(name = "egn", nullable = false)
+    private String egn;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "allergies", nullable = false)
+    private String allergies;
+    @Column(name = "immunizationStatute", nullable = false)
+    private String immunizationStatute;
+    @Column(name = "bloodType", nullable = false)
+    private Character bloodType;
+    @Column(name = "weight", nullable = false)
+    private Integer weight;
+    @Column(name = "dateOfBirth", nullable = false)
+    private Date dateOfBirth;
+    @Column(name = "laboratoryResult")
+    private String laboratoryResult;
+    @Column(name = "illness")
+    private String illness;
+
+    public Patient() {
+    }
+
+    public Patient(String name, String egn, String email, String allergies, String immunizationStatute, Character bloodType,
+                    Integer weight, Date dateOfBirth) {
+        this.name = name;
+        this.egn = egn;
+        this.email = email;
+        this.allergies = allergies;
+        this.immunizationStatute = immunizationStatute;
+        this.bloodType = bloodType;
+        this.weight = weight;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEgn() {
+        return egn;
+    }
+
+    public void setEgn(String egn) {
+        this.egn = egn;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+
+    public String getImmunizationStatute() {
+        return immunizationStatute;
+    }
+
+    public void setImmunizationStatute(String immunizationStatute) {
+        this.immunizationStatute = immunizationStatute;
+    }
+
+    public Character getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(Character bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getLaboratoryResult() {
+        return laboratoryResult;
+    }
+
+    public void setLaboratoryResult(String laboratoryResult) {
+        this.laboratoryResult = laboratoryResult;
+    }
+
+    public String getIllness() {
+        return illness;
+    }
+
+    public void setIllness(String illness) {
+        this.illness = illness;
+    }
+}
+
