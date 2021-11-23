bbbbe25ee3bbc9ed03d3442a6c8883b691969725
ManoelaYordanova <manoela.yordanova99@gmail.com>
2021-11-23 20:01:57 +0200

    made changes in access code

--- server/src/main/java/com/group12/ElectronicHealthRecords/repositories/AccessCodeRepository.java
+++ server/src/main/java/com/group12/ElectronicHealthRecords/repositories/AccessCodeRepository.java
@@ -3,6 +3,6 @@ package com.group12.ElectronicHealthRecords.repositories;
 import com.group12.ElectronicHealthRecords.entities.AccessCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccessCodeRepository extends JpaRepository {
+public interface AccessCodeRepository extends JpaRepository<AccessCode, String> {
     AccessCode findByAccessCode(String accessCode);
 }
