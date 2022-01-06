8caa4332cfcaa3a68ce74c93d72ef276cb338dda
Hristo Mitsev <dakillaroshun@gmail.com>
2022-01-06 18:55:31 +0200

    Modify ExaminationService.java

--- server/src/main/java/com/group12/ElectronicHealthRecords/repositories/ExaminationRepository.java
+++ server/src/main/java/com/group12/ElectronicHealthRecords/repositories/ExaminationRepository.java
@@ -3,10 +3,11 @@ package com.group12.ElectronicHealthRecords.repositories;
 import com.group12.ElectronicHealthRecords.entities.Examination;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface ExaminationRepository extends JpaRepository<Examination, Long> {
-    @Query("SELECT e, p FROM Examination e INNER JOIN Prescription p ON e.patient.egn = :patientEgn AND e.prescription.id = p.id")
-    List<Examination> findAllPatientExaminations(String patientEgn);
+    @Query(value = "SELECT * FROM examination e INNER JOIN patient p ON p.egn = e.patient_egn INNER JOIN prescription c ON c.id = e.prescription_id WHERE :pegn = p.egn", nativeQuery = true)
+    Optional<Examination> findAllPatientExaminations(@Param("pegn") String patientEgn);
 }
